2dc6de1da0256b525dc011f52a87305445498463
yperez0914 <ms.yeni.a.perez@gmail.com>
2020-11-11 22:52:35 -0500

    calculate total_votes_list

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,48 +16,82 @@
 
 import os
 import csv
+import math
 #Path to collect data from the Resources folder
 election_data = os.path.join(".\Resources\election_data.csv")
-with open(election_data, encoding = "utf-8") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ",")
+#with open(election_data, encoding = "utf-8") as csvfile:
+  #  csvreader = csv.reader(csvfile, delimiter = ",")
 #The total number of votes cast in the dataset
 with open(election_data, 'r', encoding = "utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+    csvheader = next(csvreader)
     total_votes = 0
     Kahn_votes = 0
+    Correy_votes = 0
+    Li_votes = 0
+    O_Tooley_votes = 0
+    candidates = []
+    votes = []
+    total_votes_list = []
+    election_results = []
+    Kahn_percentage = []
     for row in csvreader:
         voter_id = row[0]
         county = row[1]
         candidate = row[2]
+        votes.append(candidate)
+        if candidate not in candidates:
+            candidates.append(candidate)
+        
         total_votes += 1
+
+    print("Election Results")
+    print("--------------------------------------------------")
     print(total_votes)
+    print("--------------------------------------------------")
+    for candidate in candidates:
+        count = 0
+        for vote in votes:
+            if candidate == vote:
+                count = count + 1
+        total_votes_list.append(count)
+        print(candidate,count)
+
+
 
 #complete list of candidates 
-    candidates = []
-    Kahn_percentage = []
-    Kahn_votes = 0
-
+    
     for x in candidate:
         if x not in candidates:
             candidates.append(x)
+            #election_results = candidates + str(count)
+            #print(election_results)
     #print(candidates)
-    #Kahn_votes = []
-    #Correy_votes = []
-    #Li_votes = []
-    #O_Tooley_votes = [] 
-        if candidate == "Kahn": 
-            Kahn_votes += 1
-            Kahn_percentage = (Kahn_votes/total_votes) * 100
-    print(f"Kahn: {Kahn_percentage}")
-#if candidate == "Correy":
-#        number_of_votes += 1
-#print(f"{candidates[1]} + ": " + {Correy_percentage}")
- #   if candidate == "Li":
-  #      number_of_votes += 1
-#print(f"{candidates[2]} + ": " + {Li_percentage}")
- #   if candidate == "O'Tooley":
-  #      number_of_votes += 1
-#print(f"{candidates[3]} + ": " + {O_Tooley_Percentage}")
+ 
+ #The percentage of votes each candidate won
+    Kahn_percentage = (total_votes_list[0]/total_votes) * 100
+    #Kahn_percentage_format = math.trunc(Kahn_percentage, 3)
+    Correy_percentage = (total_votes_list[1]/total_votes) * 100
+    Li_percentage = (total_votes_list[2]/total_votes) * 100
+    O_Tooley_percentage = (total_votes_list[3]/total_votes) * 100
+    #print(f"Kahn: {Kahn_percentage_format}% ({total_votes_list[0]})")
+    #print(f"Correy: {Correy_percentage} ({Correy_votes})")
+    #print(f"Li: {Li_percentage} ({Li_votes})")
+    #print(f"O'Tooley: {O_Tooley_percentage} ({O_Tooley__votes})")
+    #print("--------------------------------------------------")
+#The winner of the election based on popular vote
+#election_results ={
+#   Kahn: "",
+#   Correy: "",
+#   Li: "",
+#   O'Tolley: "",
+# }
+#election_winner = max(election_results, key=election_results.get)
+print("-------------------------------------------------")
+#print(f"Winner: {election_winner}")
+        
+
+
 
 
 
