4dedfac3fd9861f410501bf19749aa5944e4150b
yperez0914 <ms.yeni.a.perez@gmail.com>
2020-11-13 18:20:49 -0500

    Finished product and created output in Analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,26 +1,11 @@
-'''* In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
-
-* You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
-
-  * The total number of votes cast
-
-  * A complete list of candidates who received votes
-
-  * The percentage of votes each candidate won
-
-  * The total number of votes each candidate won
-
-  * The winner of the election based on popular vote.
-
-* As an example, your analysis should look similar to the one below:'''
-
+#Import modules
 import os
 import csv
+
 #Path to collect data from the Resources folder
 election_data = os.path.join(".\Resources\election_data_full.csv")
-#with open(election_data, encoding = "utf-8") as csvfile:
-  #  csvreader = csv.reader(csvfile, delimiter = ",")
-#The total number of votes cast in the dataset
+
+#Read in the CSV file
 with open(election_data, 'r', encoding = "utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csvheader = next(csvreader)
@@ -31,21 +16,21 @@ with open(election_data, 'r', encoding = "utf-8") as csvfile:
     votes = []
     candidate_votes = 0
     total_votes_list = []
+
+#Loop through the data
     for row in csvreader:
         voter_id = row[0]
         county = row[1]
         candidate = row[2]
         votes.append(candidate)
-#A complete list of candidates who received votes
+
+#Calculate the total number of votes cast in the dataset
+        total_votes += 1
+
+#Create a complete list of candidates who received votes and the number of votes received
         if candidate not in candidates:
             candidates.append(candidate)
         
-        total_votes += 1
-
-    print("Election Results")
-    print("--------------------------------------------------")
-    print(F"Total Votes: {total_votes}")
-    print("--------------------------------------------------")
     for candidate in candidates:
         count = 0
         for vote in votes:
@@ -53,7 +38,7 @@ with open(election_data, 'r', encoding = "utf-8") as csvfile:
                 count = count + 1
         total_votes_list.append(count)
         
- #The percentage of votes each candidate won
+#Calculate the percentage of votes each candidate won
     Kahn_percentage = round(((total_votes_list[0]/total_votes) * 100),2)
     Kahn_percentage_format = format((Kahn_percentage), '.3f')
     Correy_percentage = round(((total_votes_list[1]/total_votes) * 100),2)
@@ -62,35 +47,27 @@ with open(election_data, 'r', encoding = "utf-8") as csvfile:
     Li_percentage_format = format((Li_percentage), '.3f')
     O_Tooley_percentage = round(((total_votes_list[3]/total_votes) * 100),2)
     O_Tooley_percentage_format = format((O_Tooley_percentage), '.3f')
-   
-    print(f"{candidates[0]}: {Kahn_percentage_format}% ({total_votes_list[0]})")
-    print(f"{candidates[1]}: {Correy_percentage_format}% ({total_votes_list[1]})")
-    print(f"{candidates[2]}: {Li_percentage_format}% ({total_votes_list[2]})")
-    print(f"{candidates[3]}: {O_Tooley_percentage}% ({total_votes_list[3]})")
-    print("--------------------------------------------------")
-#The winner of the election based on popular vote
-    #for candidate_votes in total_votes_list:    
-        #Kahn_votes = total_votes_list[0]
-        #if Kahn_votes > possible_winner:
-            #possible_winner = Kahn_votes
-            #if Correy_votes > Kahn_votes:
-               # possible_winner = Correy_votes
-    #print(f"{Kahn_votes}")
 
+#Create a dictionary with election results 
 election_results ={
-    "Kahn": total_votes_list[0],
-    "Correy": total_votes_list[1],
-    "Li": total_votes_list[2],
-    "O'Tolley": total_votes_list[3],
+    candidates[0]: total_votes_list[0],
+    candidates[1]: total_votes_list[1],
+    candidates[2]: total_votes_list[2],
+    candidates[3]: total_votes_list[3],
  }
+
+#Determine the winner of the election based on popular vote 
 election_winner = max(election_results, key=election_results.get)
+
+#Print Results
+print("Election Results")
+print("--------------------------------------------------")
+print(F"Total Votes: {total_votes}")
+print("--------------------------------------------------")
+print(f"{candidates[0]}: {Kahn_percentage_format}% ({total_votes_list[0]})")
+print(f"{candidates[1]}: {Correy_percentage_format}% ({total_votes_list[1]})")
+print(f"{candidates[2]}: {Li_percentage_format}% ({total_votes_list[2]})")
+print(f"{candidates[3]}: {O_Tooley_percentage_format}% ({total_votes_list[3]})")
+print("--------------------------------------------------")
 print(f"Winner: {election_winner}")
 print("--------------------------------------------------")
-        
-
-
-
-
-
-
-
