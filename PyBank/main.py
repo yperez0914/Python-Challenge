09d9d94fe9a42d3c5b0cf21eec6f946eb5d7d0b5
yperez0914 <ms.yeni.a.perez@gmail.com>
2020-11-13 18:54:10 -0500

    Finished product and moved main out of Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,10 @@
 #Import Modules
 import os
 import csv 
+
 #Path to collect data from the Resources folder
-budget_data_csv = os.path.join("./Resources/budget_data_full.csv")
+budget_data_csv = os.path.join("./Resources/budget_data.csv")
+
 #Read in the CSV file
 with open(budget_data_csv, 'r', encoding = "utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
@@ -21,36 +23,40 @@ with open(budget_data_csv, 'r', encoding = "utf-8") as csvfile:
     total_months += 1
     net_total += int(firstrow[1])
     prev_rev += int(firstrow[1])
-    #Loop through the data
+
+#Loop through the data
     for row in csvreader:
-      #Calculate the total number of months included in the dataset 
+
+#Calculate the total number of months included in the dataset 
       date = row[0]
       total_months += 1
 
-      #Calculate the net total amount of "Profit/Losses" over the entire period
+#Calculate the net total amount of "Profit/Losses" over the entire period
       profit_loss = int(row[1])
       net_total = net_total + profit_loss
       
-      #Calculate the changes in "Profit/Losses" over the entire period & count the number of changes
+#Calculate the changes in "Profit/Losses" over the entire period & count the number of changes
       rev_change = profit_loss - prev_rev
       num_changes += 1
       prev_rev = profit_loss
 
-      #Find the average of those changes
+#Find the average of those changes
       rev_change_total += rev_change
       rev_average = round((rev_change_total/num_changes),2)
       
-      #Calculate the greatest increase & in profits (date and amount) over the entire period
+#Calculate the greatest increase & in profits (date and amount) over the entire period
       if rev_change >= greatest_increase:
         date_increase = date
         greatest_increase = rev_change
         greatest_increase_format = "${}".format(greatest_increase)
-      #Calculate the greatest decrease in losses (date and amount) over the entire period
+        
+#Calculate the greatest decrease in losses (date and amount) over the entire period
       if rev_change < greatest_decrease:
         date_decrease = date
         greatest_decrease = rev_change
         greatest_decrease_format = "${}".format(greatest_decrease)
-                
+
+#Print Results
 print("Financial Analysis")
 print("----------------------------------------------------")      
 print(f"Total Months: {total_months}")
